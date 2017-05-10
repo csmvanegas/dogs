bb33b5b9962fa4a45e7dab343f8189d1fd82f20d
Giselle Vanegas <vanegasgisellenyc@gmail.com>
2017-05-10 11:22:45 -0400

    nav
    end of class

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
+  get 'contact/index'
+
+  get 'gallery/index'
+
+  get 'dogs/index'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
